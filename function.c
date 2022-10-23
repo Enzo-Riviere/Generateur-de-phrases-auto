96125e9e5db8326ae62fb2cdd1226b7b17c29316
quentin <quentin.damois@efrei.net>
2022-10-23 17:37:30 +0200

    creation of typeIdentify.

--- function.c
+++ function.c
@@ -3,3 +3,31 @@
 //
 
 #include "function.h"
+
+char* typeIndentify(char** ligne) {
+    /*
+     * char** ligne : correspond à une ligne du du dictionnaire.
+     * return char* esst le nom du type.
+     */
+    //on prend le troisième caractère de la troisième chaine de caracctères qui à chaque fois propre à chaque type.
+    char important_char = ligne[2][3];
+    switch(important_char) {
+        case 'm' : {
+            char res[] = "Nom";
+            break;
+        }
+        case 'r': {
+            char res[] = "Verbe";
+            break;
+        }
+        case 'j': {
+            char res[] = "Adjectif";
+            break;
+        }
+        case 'v': {
+            char res[] = "Adverbe";
+            break;
+        }
+        return res;
+    }
+}
