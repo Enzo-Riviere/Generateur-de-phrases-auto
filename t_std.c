12010f3fe55f481fe543a2af9945fda101528493
Yael TAHAR <yael.tahar@efrei.net>
2022-10-30 11:46:26 +0100

    Création des fonctions qui touche aux cellules ainsi qu'aux std_list, surtout celles pour les lettres, car beug pour les mots (voir comment gérer les chaines de caractères mot[])

--- t_std.c
+++ t_std.c
@@ -2,4 +2,63 @@
 // Created by yael1 on 25/10/2022.
 //
 
+#include <malloc.h>
 #include "t_std.h"
+
+//Fonction qui créer une cellule stockant une lettre
+//Elle prend en paramètre un caractère qui est la lettre à stocker dans la cellule, er retourne donc une cellule
+p_cell_lettre createCellLettre(char lettre){
+    p_cell_lettre p_res = NULL;
+    p_res = (p_cell_lettre) malloc (sizeof(t_cell_lettre));
+    if (p_res != NULL){
+        p_res->value = lettre;
+        p_res->next = NULL;
+    }
+    return p_res;
+}
+
+
+//Fonction qui créer une cellule stockant une mot
+//Elle prend en paramètre une chaine de caractère qui est le mot à stocker dans la cellule, er retourne donc une cellule
+/*p_cell_mot createCellMot(char mot[100]){
+    p_cell_mot p_res = NULL;
+    p_res = (p_cell_mot) malloc (sizeof(t_cell_mot));
+    if (p_res != NULL){
+        p_res->value = mot[100];
+        p_res->next = NULL;
+    }
+    return p_res;
+}*/
+
+
+//Fonction qui créer une liste de "lettre" vide
+//Elle ne prend rien en parametre et retourne une liste de lettre (t_std_list_lettre)
+t_std_list_lettre createEmptylistLettre(){
+    t_std_list_lettre maListVide;
+    maListVide.head = NULL;
+    return maListVide;
+}
+
+
+//Fonction qui créer une liste de "mot" vide
+//Elle ne prend rien en paramètre et retourne une liste de mot (t_std_list_mot)
+t_std_list_mot createEmptylistMot(){
+    t_std_list_mot maListVide;
+    maListVide.head = NULL;
+    return maListVide;
+}
+
+
+//Fonction qui permet d'ajouter une nouvelle une nouvelle cellule dans une liste de lettre
+//Elle prend en paramètre la liste de lettre à laquelle on veut ajouter une cellule, ainsi que la lettre à mettre dans cette nouvelle cellule. Elle ne retourne rien.
+void chainageLettre(t_std_list_lettre *listeLettre, char lettre){
+    p_cell_lettre nouvelle_cellule;
+    nouvelle_cellule = createCellLettre(lettre);
+
+    if (listeLettre->head == NULL){
+        listeLettre->head = nouvelle_cellule;
+    }else{
+        nouvelle_cellule->next = listeLettre->head;
+        listeLettre->head = nouvelle_cellule;
+    }
+}
--- t_std.h
+++ t_std.h
@@ -5,17 +5,17 @@
 #ifndef GENERATEUR_DE_PHRASES_AUTO_T_STD_H
 #define GENERATEUR_DE_PHRASES_AUTO_T_STD_H
 
-//Création de la structure d'une cellule pour une chaine de caractère :
+//Création de la structure d'une cellule pour une chaine de caractère (stockage des mots fléchies) :
 typedef struct{
     char value[1000];
     struct p_cell_mot *next;
-}*p_cell_mot;
+}*p_cell_mot, t_cell_mot;
 
-//Création de la structure d'une cellule pour un mot :
+//Création de la structure d'une cellule pour un mot (stockage des lettres de l'alphabet dans les arbres):
 typedef struct{
     char value;
     struct p_cell_mot *next;
-}*p_cell_lettre;
+}*p_cell_lettre, t_cell_lettre;
 
 
 //Création de la structure d'une liste simple pour stocker des cellules avec des chaines de caractères :
@@ -27,6 +27,15 @@ typedef struct{
 typedef struct{
     p_cell_lettre head;
 } t_std_list_lettre;
+
+
+p_cell_lettre createCellLettre(char);
+p_cell_mot createCellMot(char[]);
+
+t_std_list_lettre createEmptylistLettre();
+t_std_list_mot createEmptylistmot();
+
+void chainageLettre(t_std_list_lettre* ,char);
 
 
 
