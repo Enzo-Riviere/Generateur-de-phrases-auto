de5b8bc95fdb9140ca18f36924355819f4501577
quentin <quentin.damois@efrei.net>
2022-11-01 21:52:10 +0100

    preaptive modification of cell of string in flechie.c.

--- flechie.c
+++ flechie.c
@@ -3,3 +3,32 @@
 //
 
 #include "flechie.h"
+#include "node.h"
+#include "t_std.h"
+/*
+mot* creer_mot(char mot_court[]) {
+    mot *temp;
+    temp = (mot*) malloc(sizeof(mot));
+    temp->nb_flechie = 1;
+    temp->nom_mot = createEmptylistmot()
+}
+*/
+//void ajout_flech(p_node pn, char mot_court[], char flechie[], char type[]) {
+    /*
+     * Cette fonction sert à jouter le mot fléchie à au dernier caratère du mot de base.
+     * Cette fonction est appelée dans void ajout_mot(t_tree* arbre, char mot_court[], char flechie[], char type[])
+     * p_node pn : pointeur sur le noeud correspondant au dernier caractère du mot sous sa forme basique
+     * char mot_court[] : chaine de charactère contenant
+     * char flechie[] : la forme fléchie du mt
+     * char type[] : chaine de carcatère contenant tous les indormation sur le mot fléchie. exemple : "Nom:Mas+SG"
+     * note : l'exemple précédent s'applique uniquement pour les noms et est unquement pour désigner la chaine de carctère qui est constiruer de tous les carctère entre \t et \n dans le fichier .txt
+     */
+    /*
+    if (!(pn->fin_mot)) {
+        pn->fin_mot = 1;
+        pn->si_fin_mot = (mot*) malloc(sizeof(mot));
+    }
+    pn->si_fin_mot->
+
+}
+*/
