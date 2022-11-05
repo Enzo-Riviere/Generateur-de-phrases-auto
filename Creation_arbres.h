30462bf80ebb6643ed6b7b6cf4f0f0f11e5e2d8e
quentin <quentin.damois@efrei.net>
2022-11-05 18:06:35 +0100

    The file Creation_arbres.h now include the file arbre.h.

--- Creation_arbres.h
+++ Creation_arbres.h
@@ -4,6 +4,7 @@
 
 #ifndef GENERATEUR_DE_PHRASES_AUTO_CREATION_ARBRES_H
 #define GENERATEUR_DE_PHRASES_AUTO_CREATION_ARBRES_H
+#include "arbre.h"
 
 char typeIndentify(char *ligne);
 void creation_arbres();
