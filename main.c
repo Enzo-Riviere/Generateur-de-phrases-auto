dcb76b8277ee966ff60a7cc89c31016f886ff7f5
quentin damois <quentin.damois@efrei.net>
2022-11-11 19:18:03 +0100

    changed main

--- main.c
+++ main.c
@@ -22,8 +22,10 @@ int main() {
     //t_tree arbre_nom, arbre_verb, arbre_adj, arbre_adve;
     //genPhraseAleat(*(arbre_mot), *(arbre_mot + 2), *(arbre_mot + 3), *(arbre_mot + 1), 2);
     printf("\n");
+    /*
     flechies tempo;
     tempo = obtFlechNom(*genMotAleat(arbre_mot));
     printf(tempo.nom_mot);
-    return 0;
+    */
+     return 0;
 }
