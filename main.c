439bc72c83130c1d68f2f4fa3951723fd6177ba7
quentin damois <quentin.damois@efrei.net>
2022-11-12 17:31:47 +0100

    changed the inside of main

--- main.c
+++ main.c
@@ -20,12 +20,21 @@ int main() {
     arbre_mot = creation_arbres_et_donne_vieux();
 
     //t_tree arbre_nom, arbre_verb, arbre_adj, arbre_adve;
-    genPhraseAleat(*(arbre_mot), *(arbre_mot + 2), *(arbre_mot + 3), *(arbre_mot + 1), 2);
-    /*
+    //genPhraseAleat(*(arbre_mot), *(arbre_mot + 2), *(arbre_mot + 3), *(arbre_mot + 1), 2);
+
     printf("\n");
-    flechies tempo;
-    tempo = obtFlechNom(*genMotAleat(arbre_mot));
-    printf(tempo.nom_mot);
-    */
+
+    flechies tempo_nom, tempo_adj;
+    tempo_nom = obtFlechNom(*genMotAleat(arbre_mot));
+    printf(tempo_nom.forme_grammatical.head->value);
+    printf("\n");
+    mot *adj = genMotAleat(arbre_mot + 2);
+    printf(adj->flechies.head->value);
+    printf("test de l'adj flechie\n");
+    printf("\n");
+    printf("debut du test\n");
+    tempo_adj = obtFlechAdj(tempo_nom, *adj, *(arbre_mot + 2));
+    printf("fin du test\n");
+    printf(tempo_adj.forme_grammatical.head->value);
      return 0;
 }
